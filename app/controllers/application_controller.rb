c9ff9f1625eddb7c33fc53c0cb036d1751040dd7
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-03-14 20:42:25 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
-
-
-end
+    
+    get '/' do 
+      erb :super_hero
+    end 
+    
+    post '/super_hero' do 
+   
+      @team_name = params["team"]["name"]
+      @team_motto = params["team"]["motto"]
+      @team_members = params["team"]["members"]
+      erb :team 
+    end 
+  end 
